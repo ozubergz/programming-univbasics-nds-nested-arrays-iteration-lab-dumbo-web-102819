8e798bf0f2a48de78c70010c0e411007c3a008a4
James Min <jmin499@gmail.com>
2019-10-18 20:25:53 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,28 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  joined = src.reduce([]) { |arr, elem|
+    arr.push( "I love #{elem[0]} and #{elem[1]} on my pizza")
+  }
+
+  joined
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  greater_nums = [];
+  src.map { |elem|
+    greater_nums << elem.max
+  }
+  
+  greater_nums
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  even_nums = []
+  src.map { |elem|
+    if(elem[0] % 2 == 0 && elem[1] % 2 == 0)
+      even_nums << elem[0]
+      even_nums << elem[1]
+    end
+  }
+
+  even_nums.reduce(:+)
 end
